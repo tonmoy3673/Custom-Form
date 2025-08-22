056b1896a0172f68994b0cdde3523769456bc0c1
tonmoy <tonmoy3673@gmail.com>
2025-08-22 19:06:15 +0600

    :books: added content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,50 @@
-
 function App() {
- 
-
   return (
     <>
-    <main>
-      
-    </main>
+      <main
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {/* =========== Title ======= */}
+        <header
+          style={{
+            paddingTop: "10px",
+            fontSize: "25px",
+            fontWeight: "bold",
+            color: "GrayText",
+          }}
+        >
+          Custom Form
+        </header>
+        {/* ======== Form Container ======= */}
+        <div
+          style={{
+            position: "relative",
+            background: "white",
+            border: "1px solid black",
+            padding: "2rem",
+            margin: "1rem",
+            borderRadius: "6px",
+            fontFamily: "sans-serif",
+            width: "700px",
+          }}
+        >
+          {/* ========== form ======== */}
+          <form>
+            {/* =========== display step ===== */}
+            <div style={{ position: "absolute", top: ".5rem", right: ".8rem" }}>
+              0/2
+            </div>
+          </form>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

42f7e846f1ed912d3c976afc4c5b24a01f043734
tonmoy <tonmoy3673@gmail.com>
2025-08-23 14:10:15 +0600

    :zap: completed addressForm

--- src/pages/AddressForm.tsx
+++ src/pages/AddressForm.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import FormWrapper from "./FormWrapper";
 
-const AddressForm: React.FC = () => {
+interface AddressFormProps{
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+
+type AddressFormData = AddressFormProps &{
+  updatedData:(fields:Partial<AddressFormProps>)=>void
+}
+const AddressForm: React.FC <AddressFormData>= ({street,state,zip,city,updatedData}) => {
   return (
     <FormWrapper title="Address Form">
       <label>Street</label>
-      <input type="text" required autoFocus />
+      <input type="text" required autoFocus value={street} onChange={e=>updatedData({street:e.target.value})}/>
       <label>City</label>
-      <input type="text" required />
+      <input type="text" required value={city} onChange={e=>updatedData({city:e.target.value})}/>
       <label>State</label>
-      <input type="text" required/>
+      <input type="text" required value={state} onChange={e=>updatedData({state:e.target.value})}/>
       <label>Zip</label>
-      <input type="text" required/>
+      <input type="text" required value={zip} onChange={e=>updatedData({zip:e.target.value})}/>
     </FormWrapper>
   );
 };
